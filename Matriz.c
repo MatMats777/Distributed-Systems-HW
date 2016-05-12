5ce0f3aacd5782a1652c4f512e1a390392751931
Bianca Lodoli <blodoli@icloud.com>
2016-05-12 11:10:41 -0300

    funciona

--- Matriz.c
+++ Matriz.c
@@ -8,9 +8,9 @@
 #define N 8
 
 int calculaFator(int n){
-	max_fact=1;
+	int max_fact=1;
 	while(n/max_fact>max_fact){
-			max_fact++;
+		max_fact++;
 		while(n%max_fact!=0)
 			max_fact++;
 	}
@@ -18,177 +18,203 @@ int calculaFator(int n){
 }
 
 int main(int argc, char *argv[]) {
-    // Variáveis do MPI
-    int procs;
-    int myId;
-    
-    // Outras variáveis
-    int Nx_sub, Ny_sub;
-    int X_i, X_f;
-    int Y_i, Y_f;
-    
-    int M[N][N];
-    int **sub_M;
-    int diag[N];
-    int *sub_diag;
-    int soma_p[N], soma_recv[N], soma;
-    
-    int i, j;
-    
-    MPI_Status status;
-    
-    // Parâmtros para a topologia cartesiana
-    MPI_Comm CART_COMM;
-    int dim[2], period[2], reorder;
-    int tamDims;
-    int coords[2];
-    
-    // Inicia o MPI
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &procs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &myId);
-    
-    // Definir a matriz aqui:
-    if (myId == 0){
-    // . x ---->
-    /* y */ M[0][0] = 00; M[0][1] = 01; M[0][2] = 02; M[0][3] = 03; M[0][4] = 04; M[0][5] = 05; M[0][6] = 06; M[0][7] = 07;
-    /* | */ M[1][0] = 10; M[1][1] = 11; M[1][2] = 12; M[1][3] = 13; M[1][4] = 14; M[1][5] = 15; M[1][6] = 16; M[1][7] = 17;
-    /* | */ M[2][0] = 20; M[2][1] = 21; M[2][2] = 22; M[2][3] = 23; M[2][4] = 24; M[2][5] = 25; M[2][6] = 26; M[2][7] = 27;
-    /* V */ M[3][0] = 30; M[3][1] = 31; M[3][2] = 32; M[3][3] = 33; M[3][4] = 34; M[3][5] = 35; M[3][6] = 36; M[3][7] = 37;
-            M[4][0] = 40; M[4][1] = 41; M[4][2] = 42; M[4][3] = 43; M[4][4] = 44; M[4][5] = 45; M[4][6] = 46; M[4][7] = 47;
-            M[5][0] = 50; M[5][1] = 51; M[5][2] = 52; M[5][3] = 53; M[5][4] = 54; M[5][5] = 55; M[5][6] = 56; M[5][7] = 57;
-            M[6][0] = 60; M[6][1] = 61; M[6][2] = 62; M[6][3] = 63; M[6][4] = 64; M[6][5] = 65; M[6][6] = 66; M[6][7] = 67;
-            M[7][0] = 70; M[7][1] = 71; M[7][2] = 72; M[7][3] = 73; M[7][4] = 74; M[7][5] = 75; M[7][6] = 76; M[7][7] = 77;
-        
-        for (i = 0; i < N; i++) diag[i] = M[i][i];
-    } else {
-        memset(M, 0, sizeof(int)*N*N);
-    }
-    
-    // Cria o comunicador cartesiano
-    reorder = 0;
-    tamDims = sqrt(procs);
-    for (i = 0; i < 2; ++i) {
-        period[i] = 0;
-        dim[i] = tamDims;
-    }
-    
-    MPI_Cart_create(MPI_COMM_WORLD, 2, dim, period, reorder, &CART_COMM);
+// Variáveis do MPI
+	int procs;
+	int myId;
 
-    // Captura as coordenadas do processo
-    MPI_Cart_coords(CART_COMM, myId, 2, coords);
+// Outras variáveis
+	int Nx_sub, Ny_sub;
+	int X_i, X_f;
+	int Y_i, Y_f;
 
-    // Define o tamanho das sub-matrizes
-    Nx_sub = (coords[0] < N%tamDims) ? N/tamDims + 1 : N/tamDims;   // X
-    Ny_sub = (coords[1] < N%tamDims) ? N/tamDims + 1 : N/tamDims;   // Y
-    
-    // Define as coordenadas das bordas do bloco (sub-matriz)
-    X_i = (coords[0] < N%tamDims) ? coords[0] * Nx_sub : N%tamDims * (Nx_sub + 1) + (coords[0] - N%tamDims) * Nx_sub;
-    X_f = X_i + Nx_sub - 1;
-    
-    Y_i = (coords[1] < N%tamDims) ? coords[1] * Ny_sub : N%tamDims * (Ny_sub + 1) + (coords[1] - N%tamDims) * Ny_sub;
-    Y_f = Y_i + Ny_sub - 1;
-    
-    // Distribui os blocos da matriz
-    int *p;
-    p = (int *)malloc(Nx_sub*Ny_sub*sizeof(int)); 
+	int M[N][N];
+	int **sub_M;
+	int diag[N];
+	int *sub_diag;
+	int soma_p[N], soma_recv[N], soma;
 
-    sub_M = (int **)malloc(Nx_sub * sizeof(int*)); 
-    for ( i = 0; i < Ny_sub; ++i) {
-      sub_M[i] = &(p[i*Nx_sub]);
-    }
-    
-    /*********************/
-    
-    /* create a datatype to describe the subarrays of the global array */
-    int sizes[2]    = {N, N};         
-    int subsizes[2] = {Nx_sub, Ny_sub};     
-    int starts[2]   = {X_i,Y_i};                        
-    MPI_Datatype type, subarrtype;
-    MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_INT, &type);
-    MPI_Type_create_resized(type, 0, Nx_sub*sizeof(int), &subarrtype);
-    MPI_Type_commit(&subarrtype);
+	int i, j;
 
-    int *globalptr=NULL;
-    if (myId == 0)
-        globalptr = &(M[0][0]);
+	MPI_Status status;
 
-    /* scatter the array to all processors */
-    int sendcounts[dim[0]*dim[1]];
-    int displs[dim[0]*dim[1]];
+// Parâmtros para a topologia cartesiana
+	MPI_Comm CART_COMM;
+	int dim[2], period[2], reorder;
+	int tamDims;
+	int coords[2];
 
-    if (myId == 0) {
-        for (i=0; i<procs; i++)
-            sendcounts[i] = 1;
-        int disp = 0;
-        for (i=0; i<dim[0]; i++) {
-            for (j=0; j<dim[1]; j++) {
-                displs[i*dim[0]+j] = disp;
-                disp += 1;
-            }
-            disp += ((Nx_sub)-1)*dim[0];
-        }
-    }
+// Inicia o MPI
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &procs);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myId);
+
+// Definir a matriz aqui:
+	if (myId == 0){
+// . x ---->
+/* y */ M[0][0] = 00; M[0][1] = 01; M[0][2] = 02; M[0][3] = 03; M[0][4] = 04; M[0][5] = 05; M[0][6] = 06; M[0][7] = 07;
+/* | */ M[1][0] = 10; M[1][1] = 11; M[1][2] = 12; M[1][3] = 13; M[1][4] = 14; M[1][5] = 15; M[1][6] = 16; M[1][7] = 17;
+/* | */ M[2][0] = 20; M[2][1] = 21; M[2][2] = 22; M[2][3] = 23; M[2][4] = 24; M[2][5] = 25; M[2][6] = 26; M[2][7] = 27;
+/* V */ M[3][0] = 30; M[3][1] = 31; M[3][2] = 32; M[3][3] = 33; M[3][4] = 34; M[3][5] = 35; M[3][6] = 36; M[3][7] = 37;
+		M[4][0] = 40; M[4][1] = 41; M[4][2] = 42; M[4][3] = 43; M[4][4] = 44; M[4][5] = 45; M[4][6] = 46; M[4][7] = 47;
+		M[5][0] = 50; M[5][1] = 51; M[5][2] = 52; M[5][3] = 53; M[5][4] = 54; M[5][5] = 55; M[5][6] = 56; M[5][7] = 57;
+		M[6][0] = 60; M[6][1] = 61; M[6][2] = 62; M[6][3] = 63; M[6][4] = 64; M[6][5] = 65; M[6][6] = 66; M[6][7] = 67;
+		M[7][0] = 70; M[7][1] = 71; M[7][2] = 72; M[7][3] = 73; M[7][4] = 74; M[7][5] = 75; M[7][6] = 76; M[7][7] = 77;
+
+		for (i = 0; i < N; i++) diag[i] = M[i][i];
+	} else {
+		memset(M, 0, sizeof(int)*N*N);
+	}
+
+// Cria o comunicador cartesiano
+	reorder = 0;
+	tamDims = sqrt(procs);
+	for (i = 0; i < 2; ++i) {
+		period[i] = 0;
+		dim[i] = tamDims;
+	}
+
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dim, period, reorder, &CART_COMM);
+
+// Captura as coordenadas do processo
+	MPI_Cart_coords(CART_COMM, myId, 2, coords);
+
+// Define o tamanho das sub-matrizes
+	Nx_sub = N/tamDims;   // X
+	Ny_sub = N/tamDims;   // Y
+
+// Define as coordenadas das bordas do bloco (sub-matriz)
+	X_i = N%tamDims * (Nx_sub + 1) + (coords[1] - N%tamDims) * Nx_sub;
+	X_f = X_i + Nx_sub - 1;
+
+	Y_i = N%tamDims * (Ny_sub + 1) + (coords[0] - N%tamDims) * Ny_sub;
+	Y_f = Y_i + Ny_sub - 1;
+
+// Distribui os blocos da matriz
+	int *p;
+	p = (int *)malloc(Nx_sub*Ny_sub*sizeof(int)); 
+
+	sub_M = (int **)malloc(Nx_sub * sizeof(int*)); 
+	for ( i = 0; i < Ny_sub; ++i) {
+		sub_M[i] = &(p[i*Nx_sub]);
+	}
+
+// Definição de novos tipos para o envio da matriz
+	int sizes[2]    = {N, N};         
+	int subsizes[2] = {Nx_sub, Ny_sub};     
+	int starts[2]   = {X_i,Y_i};                        
+	MPI_Datatype type, subarrtype;
+	MPI_Type_create_subarray(2, sizes, subsizes, starts, MPI_ORDER_C, MPI_INT, &type);
+	MPI_Type_create_resized(type, 0, Nx_sub*sizeof(int), &subarrtype);
+	MPI_Type_commit(&subarrtype);
+
+	int *globalptr=NULL;
+	if (myId == 0)
+		globalptr = &(M[0][0]);
+
+	int sendcounts[dim[0]*dim[1]];
+	int displs[dim[0]*dim[1]];
+
+	if (myId == 0) {
+		for (i=0; i<procs; i++)
+			sendcounts[i] = 1;
+		int disp = 0;
+		for (i=0; i<dim[0]; i++) {
+			for (j=0; j<dim[1]; j++) {
+				displs[i*dim[0]+j] = disp;
+				disp += 1;
+			}
+			disp += ((Nx_sub)-1)*dim[0];
+		}
+	}
+
+// Envio da matriz
+	MPI_Scatterv(globalptr, sendcounts, displs, subarrtype, &(sub_M[0][0]),
+		Nx_sub*Ny_sub, MPI_INT,
+		0, MPI_COMM_WORLD);
+
+// Imprime as submatrizes e matriz principal    
+	for (i = 0; i < procs; ++i) {
+		if (myId == i){
+			printf("Rank = %d\n", myId);
+			if (myId == 0) {
+				printf("Global matrix: \n");
+				for (int ii=0; ii<N; ii++) {
+					for (int jj=0; jj<N; jj++) {
+						printf("%3d ", M[ii][jj]);
+					}
+					printf("\n");
+				}
+			}
+			printf("Local Matrix:\n");
+			for (int ii=0; ii<Ny_sub; ii++) {
+				for (int jj=0; jj<Nx_sub; jj++) {
+					printf("%3d ", sub_M[ii][jj]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+
+// Envia a diagonal
+	MPI_Bcast(diag, N, MPI_INT, 0, MPI_COMM_WORLD);
+
+// Multiplica cada elemento de cada linha pelo elemento da diagonal da linha correspondente
+	for (i = 0; i < Nx_sub; ++i) {
+		for (j = 0; j < Ny_sub; ++j) {
+			sub_M[i][j] *= diag[i+X_i];
+		}
+	}
+
+// Imprime as submatrizes e matriz principal novamente
+	for (i = 0; i < procs; ++i) {
+		if (myId == i){
+			printf("Rank = %d\n", myId);
+			if (myId == 0) {
+				printf("Global matrix: \n");
+				for (int ii=0; ii<N; ii++) {
+					for (int jj=0; jj<N; jj++) {
+						printf("%3d ", M[ii][jj]);
+					}
+					printf("\n");
+				}
+			}
+			printf("Local Matrix:\n");
+			for (int ii=0; ii<Ny_sub; ii++) {
+				for (int jj=0; jj<Nx_sub; jj++) {
+					printf("%3d ", sub_M[ii][jj]);
+				}
+				printf("\n");
+			}
+			printf("\n");
+
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
 
 
-    MPI_Scatterv(globalptr, sendcounts, displs, subarrtype, &(sub_M[0][0]),
-                 Nx_sub*Ny_sub, MPI_INT,
-                 0, MPI_COMM_WORLD);
-    /*********************/
-    
-    // Imprime as submatrizes e matriz principal
-    if ((sub_diag = malloc(N * sizeof(int))) == NULL) { exit(1); }
-    
+// Soma das colunas em cada processo
+	memset(soma_p, 0, sizeof(int)*N);
+	for (i = 0; i < Nx_sub; ++i) {
+		for (j = 0; j < Ny_sub; ++j) {
+			soma_p[i + X_i] += sub_M[i][j];
+		}
+	}
+	
+// Junta as somas parciais
+	soma = 0;
+	MPI_Reduce(soma_p, soma_recv, N, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+	if (myId == 0) {
+		for (i = 0; i < N; ++i) {
+			soma += soma_recv[i];
+		}
 
-    printf("Rank = %d\n", myId);
-    if (myId == 0) {
-        printf("Global matrix: \n");
-        for (int ii=0; ii<N; ii++) {
-            for (int jj=0; jj<N; jj++) {
-                printf("%3d ", M[ii][jj]);
-            }
-            printf("\n");
-        }
-    }
-    printf("Local Matrix:\n");
-    for (int ii=0; ii<Ny_sub; ii++) {
-        for (int jj=0; jj<Nx_sub; jj++) {
-            printf("%3d ", sub_M[ii][jj]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    MPI_Barrier(MPI_COMM_WORLD);
-    
-    //Passa a diagonal
-    MPI_Bcast(diag, N, MPI_INT, 0, MPI_COMM_WORLD);
-    
-    // Multiplica cada elemento de cada linha pelo elemento da diagonal da linha correspondente
-    for (j = 0; j < Ny_sub; ++j) {
-    	for (i = 0; i < Nx_sub; ++i) {
-        	sub_M[i][j] *= diag[j];
-     	}
-     }
-     
-     // Soma das colunas em cada processo
-     memset(soma_p, 0, sizeof(int)*N);
-     for (i = 0; i < Nx_sub; ++i) {
-     for (j = 0; j < Ny_sub; ++j) {
-        soma_p[i + X_i] += sub_M[i][j];
-     }
-     }
-     // Junta as somas parciais
-     soma = 0;
-     MPI_Reduce(soma_p, soma_recv, N, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
-     if (myId == 0) {
-     for (i = 0; i < N; ++i) {
-        soma += soma_recv[i];
-     }
-     
-     printf("Resultado: %d\n", soma);
-     }
-    
-    
-    MPI_Finalize();
-    return 0;
-}
+		printf("Resultado: %d\n", soma);
+	}
+
+
+	MPI_Finalize();
+	return 0;
+	}
